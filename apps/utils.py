ed46367fd82dd5d06b625e59599e67194c16ae8e
mrmcmva <vsevolodmartynyuk@yandex.ru>
2025-11-20 10:11:34 +0300

    The image generation function has been successfully implemented and validated

--- apps/utils.py
+++ apps/utils.py
@@ -1,8 +1,26 @@
 """Утилиты для инкапсуляции проверок в тестовых модулях"""
 
+from PIL import Image
+import io
+from django.core.files.uploadedfile import SimpleUploadedFile
+
 
 def is_django_field_empty(value):
     """Специально для Django полей модели"""
     if hasattr(value, 'name'):  # FileField/ImageField
         return value.name is None
     return value is None or value == ''
+
+
+def create_test_image(self, filename='test.jpg'):
+    """Создаем реальное изображение в памяти"""
+    image = Image.new('RGB', (100, 100), color='red')
+    image_io = io.BytesIO()
+    image.save(image_io, format='JPEG')
+    image_io.seek(0)
+
+    return SimpleUploadedFile(
+        filename,
+        image_io.getvalue(),
+        content_type='image/jpeg'
+    )
