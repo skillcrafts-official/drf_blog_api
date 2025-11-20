74dd73f1a278379a70996bb4cdab731c9f2b1e49
mrmcmva <vsevolodmartynyuk@yandex.ru>
2025-11-20 08:31:05 +0300

    The test of creating a user with an empty profile has been successfully implemented and validated

--- apps/profiles/tests/test_models.py
+++ apps/profiles/tests/test_models.py
@@ -4,6 +4,7 @@
 # pylint: disable=too-few-public-methods,no-member
 
 import pytest
+from apps.utils import is_django_field_empty
 from django.db import transaction, IntegrityError
 from apps.profiles.models import Profile
 
@@ -18,15 +19,17 @@ class TestProfileModel:
         Проверяется создание профиля для пользователя
         Один профиль для одного пользователя
         """
-        # Создание пользователя на уровне моделей не должно создавать профиля
+        # Создание пользователя на уровне моделей не должно создавать профиль
         users = users_pool()
         assert not Profile.objects.filter(user=users.user1).exists()
 
         # Для одного пользователя создать можно только один профиль
-        r_profile = profile_data(users.user1.id)
+        r_profile = profile_data(for_user=users.user1)
         profile1 = Profile.objects.create(**r_profile)
         assert isinstance(profile1, Profile), f"{type(profile1) =} "
         assert profile1.user == users.user1
+        for attr in r_profile:
+            assert getattr(profile1, attr)
 
         # Повторное создание профиля должно приводить к IntegrityError
         try:
@@ -34,3 +37,15 @@ class TestProfileModel:
                 Profile.objects.create(**r_profile)
         except IntegrityError as e:
             assert 'UNIQUE' in str(e), f"{str(e) =} "
+
+    def test_create_user_empty_profile(self, users_pool, profile_data):
+        """
+        Проверяется возможность создания пустого профиля пользователя
+        """
+        users = users_pool()
+
+        r_profile = profile_data(for_user=users.user1, is_null=True)
+        profile1 = Profile.objects.create(**r_profile)
+
+        for attr in (attr for attr in r_profile if attr != 'user_id'):
+            assert is_django_field_empty(getattr(profile1, attr))
