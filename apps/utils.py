2788391145f3bf4670b0a8f0878a2264629fdef7
mrmcmva <vsevolodmartynyuk@yandex.ru>
2025-11-25 17:11:28 +0300

    The application utilies have been successfully updated to version 2025.11.0

--- apps/utils.py
+++ apps/utils.py
@@ -1,18 +1,22 @@
 """Утилиты для инкапсуляции проверок в тестовых модулях"""
 
-from PIL import Image
 import io
+from PIL import Image
+
 from django.core.files.uploadedfile import SimpleUploadedFile
 
 
 def is_django_field_empty(value):
     """Специально для Django полей модели"""
-    if hasattr(value, 'name'):  # FileField/ImageField
+    # print(value.__dict__)
+    if hasattr(value, '_state'):
+        return value.id
+    if hasattr(value, '_file'):  # FileField/ImageField
         return value.name is None
     return value is None or value == ''
 
 
-def create_test_image(self, filename='test.jpg'):
+def create_test_image(filename='test.jpg'):
     """Создаем реальное изображение в памяти"""
     image = Image.new('RGB', (100, 100), color='red')
     image_io = io.BytesIO()
