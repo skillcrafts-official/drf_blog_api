adebdb167fc1d871793e4eeba141a309607cbd98
mrmcmva <vsevolodmartynyuk@yandex.ru>
2025-11-25 17:10:06 +0300

    The unit testing suites have been successfully updated to version 2025.11.0

--- conftest.py
+++ conftest.py
@@ -1,41 +1,42 @@
 """Global conftest.py"""
+
+import tempfile
+import shutil
 import pytest
+
+from django.test import override_settings
 
 from rest_framework.test import APIClient
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from apps.accounts.models import User
+
+pytest_plugins = [
+    'apps.accounts.tests.conftest',
+    'apps.profiles.tests.conftest',
+]
 
 
-@pytest.fixture(scope='class')
-def users_pool():
-    """Пул пользователей"""
-    # pylint: disable=too-few-public-methods
-    class UsersPool:
-        """
-        Создаёт именные атрибуты пользователей
-        """
-        def __init__(self, quantity: int = 1):
-            for i in range(quantity):
-                user_data = {
-                    'email': f'testuser{i + 1}@example.com',
-                    'password': 'testpass123'
-                }
-                setattr(
-                    self, f'user{i + 1}',
-                    User.objects.create(**user_data)
-                )
+@pytest.fixture
+def temp_media():
+    """Создает временную MEDIA_ROOT которая автоматически удаляется"""
+    temp_dir = tempfile.mkdtemp()
 
-    return UsersPool
+    with override_settings(MEDIA_ROOT=temp_dir):
+        yield temp_dir
+
+    # Удаляем временную папку после теста
+    shutil.rmtree(temp_dir, ignore_errors=True)
 
 
 @pytest.fixture
 def auth_client():
     """Фабрика API клиентов с JWT токеном в заголовках"""
-    def create_auth_clint(**kwargs):
-        user = kwargs.get('user')
-        refresh = RefreshToken.for_user(user)
+    def create_auth_client(user=None):
         client = APIClient()
-        client.credentials(HTTP_AUTHORIZATION=f'Bearer {refresh.access_token}')
+        if user is not None:
+            refresh = RefreshToken.for_user(user)  # type: ignore
+            client.credentials(
+                HTTP_AUTHORIZATION=f'Bearer {refresh.access_token}'
+            )
         return client
-    return create_auth_clint
+    return create_auth_client
