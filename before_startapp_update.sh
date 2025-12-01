0c64e790a7696da79d5c93c0c1dc93ea54dd4bfd
mrmcmva <vsevolodmartynyuk@yandex.ru>
2025-12-01 14:00:38 +0300

    Tha application directory has been successfully extended

---
+++ before_startapp_update.sh
@@ -0,0 +1,14 @@
+#!bash
+
+PATH_TO_APP=$1
+
+mkdir ${PATH_TO_APP}/tests/
+cd ${PATH_TO_APP}/tests/
+touch __init__.py
+echo '"""Local conftest.py"""' > conftest.py
+echo '"""Tests for app models"""' > test_models.py
+echo '"""Tests for app serializers"""' > test_serializers.py
+echo '"""Tests for app views"""' > test_views.py
+cd ..
+echo '"""Serializers for ${PATH_TO_APP}"""' > serializers.py
+echo '"""The DRF documentation extends for app ${PATH_TO_APP}"""' > spectaculars.py
