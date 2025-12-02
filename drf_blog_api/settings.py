6032bfa960ea6f2c3f6f0caabacba4e764efd72f
mrmcmva <vsevolodmartynyuk@yandex.ru>
2025-12-02 13:51:12 +0300

    Prepare for prod

--- drf_blog_api/settings.py
+++ drf_blog_api/settings.py
@@ -222,6 +222,7 @@ SIMPLE_JWT = {
 
 # CORS настройки
 CORS_ALLOWED_ORIGINS = [
+    "https://react-blog-kappa-plum.vercel.app",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:3000",  # если используете другой порт
