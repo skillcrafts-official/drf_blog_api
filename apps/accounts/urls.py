e4578f18ba3ad6736fef19a303d0a762856011cc
mrmcmva <vsevolodmartynyuk@yandex.ru>
2025-12-18 13:51:55 +0300

    Hotfixes commit

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -42,10 +42,10 @@ urlpatterns = [
         'auth/token/verify/', TokenVerifyView.as_view(),
         name='token_verify'
     ),
-    path(
-        'auth/guest-token/', GuestTokenObtainView.as_view(),
-        name='gest_token_obtain_pair'
-    ),
+    # path(
+    #     'auth/guest-token/', GuestTokenObtainView.as_view(),
+    #     name='gest_token_obtain_pair'
+    # ),
     # path(
     #     'auth/token/refresh/', TokenRefreshView.as_view(),
     #     name='token_refresh'
