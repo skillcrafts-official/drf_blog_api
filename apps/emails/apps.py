eb389945173f0674a54e3e49d70661a082024d1f
mrmcmva <vsevolodmartynyuk@yandex.ru>
2025-12-21 15:24:43 +0300

    Hotfixes commit

--- apps/emails/apps.py
+++ apps/emails/apps.py
@@ -4,3 +4,12 @@ from django.apps import AppConfig
 class EmailsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.emails'
+
+    def ready(self):
+        try:
+            # pylint: disable=import-outside-toplevel,unused-import
+            # from apps.accounts import spectaculars  # noqa: F401
+            from apps.emails import signals       # noqa: F401
+            # from apps.accounts import handlers      # noqa: F401
+        except ImportError:
+            pass
