748fbc5b1bf7c5ea57d6def997215d3688f38e2a
mrmcmva <vsevolodmartynyuk@yandex.ru>
2025-12-26 10:47:02 +0300

    Hotfixing commit

--- apps/my_workflows/serializers.py
+++ apps/my_workflows/serializers.py
@@ -1,3 +1,5 @@
+from django.db.models import Sum
+
 from rest_framework import serializers
 
 from apps.my_workflows.models import (
@@ -21,10 +23,24 @@ class AcceptanceCriteriaSerializer(serializers.ModelSerializer):
 
 
 class TimeEntrySerializer(serializers.ModelSerializer):
+    # all_spents = serializers.SerializerMethodField()
 
     class Meta:
         model = TimeEntry
         fields = '__all__'
+
+    # def get_all_spents(self):
+    #     print(self.__dict__)
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        print(instance.__dict__)
+        representation.update(
+            **TimeEntry.objects
+            .filter(task=instance.task)
+            .aggregate(all_spents=Sum('hours_spent'))
+        )
+        return representation
 
 
 class TaskSerializer(serializers.ModelSerializer):
